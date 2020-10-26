1e50106defa56d7209870382b005cded946cf42b
shubham <shubhamkumar10516@gmail.com>
2020-10-27 00:46:50 +0530

    [Shubham]REFACTORED: Test cases modified to calculate fare for PREMIUM and NORMAL type of ride

--- src/test/java/CarInvoiceProblem/InvoiceServiceTest.java
+++ src/test/java/CarInvoiceProblem/InvoiceServiceTest.java
@@ -8,13 +8,18 @@ import org.junit.Before;
 
 public class InvoiceServiceTest {
 	InvoiceService invoiceService = null;
+	RideRepository rideRepository = null;
+	Ride[] rides = null;
 
-	//initialization of invoiceService object
+	// initialization of invoiceService object
 	@Before
 	public void loadData() {
 		invoiceService = new InvoiceService();
+		rideRepository = new RideRepository();
+		invoiceService.setRideRepository(rideRepository);
+		rides = new Ride[] { new Ride(CabRide.NORMAL, 2.0, 5), new Ride(CabRide.PREMIUM, 0.1, 1) };
 	}
-	
+
 	@Test
 	public void givenDistanceCalculateTotalFare() {
 		double distance = 2.0;
@@ -22,7 +27,7 @@ public class InvoiceServiceTest {
 		double fare = invoiceService.getFare(distance, time);
 		assertEquals(25, fare, 0);
 	}
-	
+
 	@Test
 	public void givenLessDistanceAndTimeReturnMinimumFare() {
 		double distance = 0.1;
@@ -30,22 +35,20 @@ public class InvoiceServiceTest {
 		double fare = invoiceService.getFare(distance, time);
 		assertEquals(5, fare, 0);
 	}
-	
+
 	@Test
 	public void givenMultipleRidesReturnTotalFare() {
-		Ride[] rides = { new Ride(2.0, 5), new Ride(2.0, 5), new Ride(0.1, 1) };
-		InvoiceSummary expectedSummary = new InvoiceSummary(55, 3);
+		InvoiceSummary expectedSummary = new InvoiceSummary(45, 2);
 		InvoiceSummary summary = invoiceService.getFare(rides);
 		assertEquals(expectedSummary, summary);
 	}
-	
+
 	@Test
 	public void givenUserIdAndRideReturnInvoiceSummary() {
-		String userId= "a@b.com";
-		Ride[] rides = { new Ride(2.0, 5), new Ride(2.0, 5), new Ride(0.1, 1) };
+		String userId = "a@b.com";
 		invoiceService.addRides(userId, rides);
 		InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
-		InvoiceSummary expectedSummary = new InvoiceSummary(55, 3);
+		InvoiceSummary expectedSummary = new InvoiceSummary(45, 2);
 		assertEquals(expectedSummary, summary);
 	}
 }
