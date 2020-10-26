907638d8f456e6054b4f7d8649e9412ef1e3e36e
shubham <shubhamkumar10516@gmail.com>
2020-10-26 20:30:43 +0530

    [Shubham]ADDED: Test case for minimum get fare method added

--- src/test/java/CarInvoiceProblem/InvoiceServiceTest.java
+++ src/test/java/CarInvoiceProblem/InvoiceServiceTest.java
@@ -21,4 +21,12 @@ public class InvoiceServiceTest {
 		double fare = invoiceService.getFare(distance, time);
 		assertEquals(25, fare, 0);
 	}
+	
+	@Test
+	public void givenLessDistanceAndTimeReturnMinimumFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceService.getFare(distance, time);
+		assertEquals(5, fare, 0);
+	}
 }
