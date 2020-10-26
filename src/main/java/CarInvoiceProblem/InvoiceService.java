75abcfb867e496cbffabc81b8cf74a26b45ce25f
shubham <shubhamkumar10516@gmail.com>
2020-10-26 20:33:02 +0530

    [Shubham]REFACTORED: Get fare function modified to calculate minimum fare for given distance and time

--- src/main/java/CarInvoiceProblem/InvoiceService.java
+++ src/main/java/CarInvoiceProblem/InvoiceService.java
@@ -3,10 +3,11 @@ package CarInvoiceProblem;
 public class InvoiceService {
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 	
-	// calculate total fare for distance and time
+	// calculate total fare for given distance and time
 		public double getFare(double distance, int time) {
 			double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-			return totalFare;
+			return Math.max(totalFare, MINIMUM_FARE);
 		}
 }
