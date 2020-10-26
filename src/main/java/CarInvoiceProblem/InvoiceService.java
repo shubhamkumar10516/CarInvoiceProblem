72a39051fe41c6f3d39fad23fa652ae6a678d1a2
shubham <shubhamkumar10516@gmail.com>
2020-10-26 22:01:49 +0530

    [Shubham]REFACTORED: Fare calculator method modified to return enhanced invoice

--- src/main/java/CarInvoiceProblem/InvoiceService.java
+++ src/main/java/CarInvoiceProblem/InvoiceService.java
@@ -1,13 +1,23 @@
 package CarInvoiceProblem;
 
+
 public class InvoiceService {
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
-	
+
 	// calculate total fare for given distance and time
-		public double getFare(double distance, int time) {
-			double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-			return Math.max(totalFare, MINIMUM_FARE);
-		}
+	public double getFare(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+		return Math.max(totalFare, MINIMUM_FARE);
+	}
+
+	// calculating fare for multiple rides
+	public InvoiceSummary getFare(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides)
+			totalFare += getFare(ride.getDistance(), ride.getTime());
+
+		return new InvoiceSummary(totalFare, rides.length);
+	}
 }
